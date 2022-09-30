f97f94d9479eaea46854709ba449f3064f9c3b76
Merle Roji <merle.roji@mymail.champlain.edu>
2022-09-30 14:13:37 -0400

    Prepping Agent, Cat, and Catcher for algorithms.

--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -1,13 +1,23 @@
 #ifndef AGENT_H
 #define AGENT_H
 #include "Point2D.h"
+#include <map>
 
 class World;
+
+struct queueEntry
+{
+  Point2D position;
+  int weight;
+  bool operator<(const queueEntry& rhs) const;
+};
 
 class Agent {
 public:
   explicit Agent()= default;;
   virtual Point2D Move(World*)=0;
+
+  std::map<int, std::map<int, bool>> visited;
 };
 
 #endif  // AGENT_H
--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -2,7 +2,34 @@
 #include "World.h"
 #include <stdexcept>
 
-Point2D Cat::Move(World* world) {
+Point2D Cat::Move(World* world)
+{
+  auto cat = world->getCat();
+  std::map<int, std::map<int, bool>> visited;
+
+  std::vector<queueEntry> queue;
+  // bootstrap the first element into the queue
+  queue.push_back({cat, 0});
+
+  // while we have elements to be visited, visit them
+  while (!queue.empty())
+  {
+    // fetching the first element
+    auto head = queue[0];
+    queue.erase(queue.begin());
+
+    // mark the head as visited
+    // for each neighbor
+    // - check if is not visited yet
+    // - check if is not in the queue
+    // - check if it can be visited
+    // - add to the queue adding 1 to the weight
+    // - mark the neighbor as it came from the current head we popped
+    
+
+    // here you code your win condition and whatever your navigation process is
+  }
+
   auto rand = Random::Range(0,5);
   auto pos = world->getCat();
   switch(rand){
